b1a745ddca59958cac5e1657fa4b16632612a51b
Labbaika Asri <labbaikaasri07@gmail.com>
2020-09-14 22:32:16 +0700

    Update & delete (soft delete)

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,41 +1,46 @@
 @extends('layout/main')
 
-@section('title', 'Form Insert Student Data')
+@section('title', 'Form Edit Student Data')
 
 @section('container')
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <h1 class="mt-3">Form Insert Student Data</h1>
+            <h1 class="mt-3">Form Edit Student Data</h1>
 
-            <form method="post" action="/students">
+            <form method="post" action="/students/{{$student->id}}">
+                @method('patch')
                 @csrf
                 <div class="form-group">
                     <label for="nama">Name</label>
                     <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama"
-                        placeholder="Insert Name" value="{{old('nama')}}">
+                        placeholder="Insert Name"
+                        value="@if(old('nama') != null) {{old('nama')}} @else {{$student->nama}} @endif">
                     @error('nama')<div class="invalid-feedback">{{$message}}</div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="nrp">NRP</label>
                     <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp"
-                        placeholder="Insert NRP" maxlength="10" value="{{old('nrp')}}">
+                        placeholder="Insert NRP" maxlength="10"
+                        value="@if(old('nrp') != null) {{old('nrp')}} @else {{$student->nrp}} @endif">
                     @error('nrp')<div class="invalid-feedback">{{$message}}</div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
-                        placeholder="Insert Email" value="{{old('email')}}">
+                        placeholder="Insert Email"
+                        value="@if(old('email') != null) {{old('email')}} @else {{$student->email}} @endif">
                     @error('email')<div class="invalid-feedback">{{$message}}</div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="jurusan">Major</label>
                     <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan"
-                        name="jurusan" placeholder="Insert Major" value="{{old('jurusan')}}">
+                        name="jurusan" placeholder="Insert Major"
+                        value="@if(old('jurusan') != null) {{old('jurusan')}} @else {{$student->jurusan}} @endif">
                     @error('jurusan')<div class="invalid-feedback">{{$message}}</div>@enderror
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Edit Data</button>
             </form>
 
         </div>
--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -15,8 +15,12 @@
                     <p class="card-text">{{$student->email}}</p>
                     <p class="card-text">{{$student->jurusan}}</p>
 
-                    <button type="submit" class="btn btn-primary">Edit</button>
-                    <button type="submit" class="btn btn-danger">Delete</button>
+                    <a href="{{$student->id}}/edit" class="btn btn-primary">Edit</a>
+                    <form action="{{$student->id}}" method="POST" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
                     <a href="/students" class="card-link">
                         &larr; Back
                     </a>
